6a6f1e153e087afc518e6951d1619871940c0945
marlanz <phucanhdodang1211@gmail.com>
2026-07-01 16:05:09 +0700

    configure client side better auth

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,3 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
-export const authClient = createAuthClient({});
+export const authClient = createAuthClient({
+  baseURL: typeof window !== "undefined"
+    ? window.location.origin
+    : process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "https://dashboard-ddc.vercel.app",
+});
